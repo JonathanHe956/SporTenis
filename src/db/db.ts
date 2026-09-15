3d61e9d60f2ae3096d038d10c2c47626bfd4f98f
JonathanHe956 <jona.hernandezz.esp@gmail.com>
2026-09-15 11:26:53 -0600

    correciones

--- src/db/db.ts
+++ src/db/db.ts
@@ -5,6 +5,7 @@ import 'dotenv/config';
 
 const globalForDb = globalThis as unknown as {
   conn: mysql.Pool | undefined;
+  hasListeners: boolean | undefined;
 };
 
 const connection = globalForDb.conn ?? mysql.createPool({
@@ -13,6 +14,22 @@ const connection = globalForDb.conn ?? mysql.createPool({
 
 if (process.env.NODE_ENV !== 'production') {
   globalForDb.conn = connection;
+  
+  // Apagar la conexión a la base de datos limpiamente cuando se cierra el servidor
+  if (!globalForDb.hasListeners) {
+    const closePool = async () => {
+      if (globalForDb.conn) {
+        try {
+          await globalForDb.conn.end();
+          console.log('\n[Conexiones a MySQL cerradas correctamente]');
+        } catch (e) {}
+      }
+      process.exit(0);
+    };
+    process.on('SIGINT', closePool);
+    process.on('SIGTERM', closePool);
+    globalForDb.hasListeners = true;
+  }
 }
 
 export const db = drizzle(connection, { schema, mode: 'default' });
